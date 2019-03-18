2c5acfcb619bdba03c5e02c02705b9de433007b3
Yann Guillemain <devguillemain@gmail.com>
2019-03-18 17:44:47 +0100

    Setting up the project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
@@ -16,13 +15,16 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500,700');
+$grey: #252525;
+
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Fira Sans', sans-serif;
+  color: $grey;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,125 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="content">
+    <section class="home">
+      <h3 class="name">Yann Guillemain</h3>
+      <p>Curriculum vitae intéractif</p>
+    </section>
+    <section class="profile">
+      <h3>Profile</h3>
+      <div class="profile__description">
+        <figure class="profile__picture">
+          <img src="profile.jpg" alt="Yann Guillemain">
+        </figure>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ut, exercitationem reiciendis velit asperiores et obcaecati.
+          Ab modi veniam, laudantium consectetur totam quae illo. Quam suscipit explicabo corrupti officia cum!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ut, exercitationem reiciendis velit asperiores et obcaecati.
+          Ab modi veniam, laudantium consectetur totam quae illo. Quam suscipit explicabo corrupti officia cum!
+        </p>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'HelloWorld'
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+  $main-color: #00444980;
+  .content {
+    margin-bottom: 6rem;
+  }
+  .home {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    background-image: url('/codecode.jpg');
+    background-image: linear-gradient(to right bottom, rgba(0, 68, 73, .5), rgba(0, 68, 73, .5)), url('/codecode.jpg');
+    background-size: cover;
+    background-attachment: fixed;
+
+    p {
+      font-size: 1.5rem;
+      color: white;
+      opacity: .9;
+      animation: slideRight 1s ease-in backwards;
+      animation-delay: .2s;
+    }
+  }
+  .name {
+    font-weight: 300;
+    font-size: 4rem;
+    letter-spacing: .1rem;
+    margin-bottom: 2rem;
+    color: white;
+    opacity: .9;
+    animation: slideLeft 1s ease-in backwards;
+
+    &::after {
+      content: "";
+      background-color: rgba(255, 255, 255, .5);
+      display: block;
+      margin: auto;
+      width: 70%;
+      height: 1px;
+      margin-top: 1rem;
+      animation: appears 1s ease-in-out .6s backwards;
+    }
+  }
+
+  .profile {
+    margin-top: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    h3 {
+      color: $main-color;
+      font-size: 2.5rem;
+      font-weight: 300;
+      margin-bottom: 2rem;
+    }
+
+    .profile__description {
+      display: flex;
+      align-items: center;
+      width: 50%;
+
+      & .profile__picture {
+        flex: 0 0 30%;
+        clip-path: circle(46% at 50% 50%);
+        margin-right: 3rem;
+
+        & img {
+          transition: all .2s;
+          width: 100%;
+
+          &:hover {
+            transform: scale(.93);
+          }
+        }
+      }
+    }
+  }
+
+  @keyframes slideLeft {
+    0% { transform: translateX(-40vw); opacity: 0 }
+    80% { transform: translateX(5vw) }
+    100% { transform: translateX(0); opacity: 1 }
+  }
+  @keyframes slideRight {
+    0% { transform: translateX(30vw); opacity: 0 }
+    70% { transform: translateX(-5vw) }
+    90% { transform: translateX(2vw) }
+    100% { transform: translateX(0); opacity: 1 }
+  }
+  @keyframes appears {
+    0% { opacity: 0 }
+    100% { opacity: 1 }
+  }
 </style>
